79fd01e33a0e77cbdc3e0ecfc6d8385962ef822d
Roua Mechleoui <mechleouiroua9@gmail.com>
2023-06-04 21:10:30 +0100

    design+confirmation+update ordonnace

---
+++ src/com/esprit/gui/AjouterMedecinController.java
@@ -0,0 +1,128 @@
+package com.esprit.gui;
+
+import com.esprit.entities.Medecin;
+import com.esprit.entities.Ordonnance;
+import com.esprit.entities.Specialite;
+import com.esprit.services.ServiceMedecin;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Optional;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.util.Callback;
+
+public class AjouterMedecinController implements Initializable {
+
+    @FXML
+    private TextField idNom;
+    @FXML
+    private ComboBox<String> idspecialite;
+    @FXML
+    private TextField idprenom;
+    @FXML
+    private TableView<Medecin> table;
+    @FXML
+    private TableColumn<Medecin, String> nom;
+    @FXML
+    private TableColumn<Medecin, String> prenom;
+   @FXML
+    private TableColumn<Medecin, String>specialite;
+    @FXML
+    private TextField email;
+    @FXML
+    private TableColumn<Medecin, String> email_col;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        nom.setCellValueFactory(new PropertyValueFactory<>("nom"));
+        prenom.setCellValueFactory(new PropertyValueFactory<>("prenom"));
+        specialite.setCellValueFactory(new PropertyValueFactory<>("specialite"));
+        email_col.setCellValueFactory(new PropertyValueFactory<>("email"));
+      
+    idspecialite.getItems().addAll("CARDIOLOGISTE","DERMATOLOGISTE","GYNECOLOGUE","PEDIATRE");
+
+        ServiceMedecin sm = new ServiceMedecin();
+        ObservableList<Medecin> medecinList = FXCollections.observableArrayList(sm.afficher());
+        table.setItems(medecinList);
+
+        TableColumn<Medecin, Void> colBtn = new TableColumn<>("Suprimer");
+        Callback<TableColumn<Medecin, Void>, TableCell<Medecin, Void>> cellFactory = new Callback<TableColumn<Medecin, Void>, TableCell<Medecin, Void>>() {
+            @Override
+            public TableCell<Medecin, Void> call(final TableColumn<Medecin, Void> param) {
+                final TableCell<Medecin, Void> cell = new TableCell<Medecin, Void>() {
+
+                    private final Button btn = new Button("Suprimer");
+
+                    {
+                        btn.setOnAction((ActionEvent event) -> {
+                            Medecin data = getTableView().getItems().get(getIndex());
+                            System.out.println("selectedData: " + data);
+                            sm.supprimer(data);
+                            medecinList.remove(data);
+                        });}
+
+                    @Override
+                    public void updateItem(Void item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if (empty) {
+                            setGraphic(null);
+                        } else {
+                            setGraphic(btn);
+                        }
+                    }
+                };
+                return cell;
+            }
+        };
+
+        colBtn.setCellFactory(cellFactory);
+        table.getColumns().add(colBtn);
+        
+    }
+
+    @FXML
+    private void ajout(ActionEvent event) throws IOException {
+        ServiceMedecin sm = new ServiceMedecin();
+        Medecin medecin = new Medecin(idNom.getText(), idprenom.getText(), Specialite.valueOf(idspecialite.getSelectionModel().getSelectedItem()),email.getText());
+        Dialog<ButtonType> dialog = new Dialog<>();
+        dialog.setTitle("Confirmation Ajout");
+        dialog.setHeaderText("Voulez-vous vraiment ajouter ce medecin");
+
+        ButtonType buttonTypeYes = new ButtonType("Oui", ButtonBar.ButtonData.YES);
+        ButtonType buttonTypeNo = new ButtonType("Non", ButtonBar.ButtonData.NO);
+
+        dialog.getDialogPane().getButtonTypes().addAll(buttonTypeYes, buttonTypeNo);
+        Optional<ButtonType> result = dialog.showAndWait();
+
+        if (result.isPresent() && result.get() == buttonTypeYes) {
+        sm.ajouter(medecin);
+
+        table.getItems().add(medecin);
+
+        idNom.clear();
+        idprenom.clear();
+        email.clear();
+        idspecialite.setValue(null);
+    }
+        else{
+            dialog.close();
+            
+        }
+}
+}
---
+++ src/com/esprit/services/IService.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.esprit.services;
+
+import java.util.List;
+
+public interface IService<T> {
+
+    public void ajouter(T t);
+    public void modifier(T t);
+    public void supprimer(T t);
+    public List<T> afficher();
+    
+}
