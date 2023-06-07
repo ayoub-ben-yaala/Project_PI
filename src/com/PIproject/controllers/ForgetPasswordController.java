995f62cee4486f4d385c96070c7d980860e7bae6
ayou-ben-yaala <ayoubbenyaala@gmail.com>
2023-06-07 16:39:10 +0200

    Send Email with secure Code

--- src/com/PIproject/controllers/ForgetPasswordController.java
+++ src/com/PIproject/controllers/ForgetPasswordController.java
@@ -4,8 +4,15 @@
  */
 package com.PIproject.controllers;
 
+import com.mysql.cj.Session;
+import com.mysql.cj.protocol.Message;
+import com.sun.xml.internal.messaging.saaj.packaging.mime.MessagingException;
+import com.sun.xml.internal.org.jvnet.mimepull.MIMEMessage;
 import java.io.IOException;
+import java.net.Authenticator;
+import java.net.PasswordAuthentication;
 import java.net.URL;
+import java.util.Properties;
 import java.util.Random;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -18,6 +25,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import sun.rmi.transport.Transport;
 
 /**
  * FXML Controller class
@@ -49,8 +57,36 @@ public class ForgetPasswordController implements Initializable {
     
     return code;
 }
-    
-    
+    /*
+    public static void sendSecurityCode(String recipientEmail, String securityCode) {
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(properties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("ayoubbenyaala@gmail.com", "ayoubbenyaala@gmail.com@@");
+            }
+        });
+
+        try {
+            Message message = new MIMEMessage(session);
+            message.setFrom(new InternetAddress(EMAIL_USERNAME));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipientEmail));
+            message.setSubject("Code de sécurité");
+            message.setText("Votre code de sécurité est : " + securityCode);
+
+            Transport.send(message);
+
+            System.out.println("E-mail envoyé avec succès !");
+        } catch (MessagingException e) {
+            System.out.println("Erreur lors de l'envoi de l'e-mail : " + e.getMessage());
+        }
+    }*/
+      
     @FXML
      private boolean verifyCode(int enteredCode) {
     return enteredCode == code;
@@ -72,12 +108,12 @@ public class ForgetPasswordController implements Initializable {
              primaryStage.setScene(scene);
     } else {
       showAlert(Alert.AlertType.ERROR, "Code Invalide", "", "Veuillez vérifier le code envoyer a votre mail.");
-FXMLLoader loader = new FXMLLoader(getClass().getResource("../views/Login.fxml"));
+/*FXMLLoader loader = new FXMLLoader(getClass().getResource("../views/Login.fxml"));
             Parent root = loader.load();
             Scene scene = new Scene(root);
             Stage primaryStage = (Stage) send.getScene().getWindow();
             primaryStage.setScene(scene);  
-             primaryStage.setScene(scene);
+             primaryStage.setScene(scene);*/
     }
         
     }
